e254701278ff9c26da97d1c03f9e7de17171efd1
xiefei <applemechanic@163.com>
2020-10-13 21:53:47 +0800

    add phantomReference

--- learn-project-concurrent/src/main/java/com/tfc/learn/TestPhantomReference.java
+++ learn-project-concurrent/src/main/java/com/tfc/learn/TestPhantomReference.java
@@ -1,4 +1,13 @@
 package com.tfc.learn;
 
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+
 public interface TestPhantomReference {
+    ReferenceQueue<Object> referenceQueue=new ReferenceQueue<>();
+
+    static void main(String[] args) {
+        PhantomReference<Object> phantomReference=new PhantomReference<>(new Object(),referenceQueue);
+        System.out.println(phantomReference.get());
+    }
 }
