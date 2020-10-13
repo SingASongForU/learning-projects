ca2c40bce5a5f0990b6b81cff78c61b07c7a0195
xiefei <applemechanic@163.com>
2020-10-13 22:18:05 +0800

    add phantomReference

--- learn-project-concurrent/src/main/java/com/tfc/learn/TestPhantomReference.java
+++ learn-project-concurrent/src/main/java/com/tfc/learn/TestPhantomReference.java
@@ -1,13 +1,46 @@
 package com.tfc.learn;
 
+import com.google.common.collect.Lists;
+import com.tfc.learn.lock.TestAtomic;
+
 import java.lang.ref.PhantomReference;
 import java.lang.ref.ReferenceQueue;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public interface TestPhantomReference {
-    ReferenceQueue<Object> referenceQueue=new ReferenceQueue<>();
+public class TestPhantomReference {
+    public static final List<Object> LIST= Lists.newArrayList();
+    public static ReferenceQueue<Object> referenceQueue=new ReferenceQueue<>();
 
-    static void main(String[] args) {
+    public void collect(){
+        while(true){
+            Object o=referenceQueue.poll();
+            if(o!=null){
+                System.out.println("开始回收堆外内存，因为对象："+o+"被gc回收了");
+                System.exit(0);
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
         PhantomReference<Object> phantomReference=new PhantomReference<>(new Object(),referenceQueue);
         System.out.println(phantomReference.get());
+
+//        new Thread(()->{
+//            while(true){
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                byte[] bytes=new byte[1*1024*1024];
+//                LIST.add(bytes);
+//                System.out.println(phantomReference.get());
+//            }
+//        }).start();
+//
+//        new Thread(new TestPhantomReference()::collect).start();
     }
 }
